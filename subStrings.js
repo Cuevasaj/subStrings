cadb6a8ab888cfa086d2bb85f2a9f4111575d926
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2023-01-10 11:45:53 -0700

    Adding current files -Work In Progress-

---
+++ subStrings.js
@@ -0,0 +1,28 @@
+/*making function to take in a word as the first argument then 
+strings as valid substrings
+*/
+const subString = (phrase, dict) => {
+  for (let i = 0; i < dict; i++);
+  return {
+    subString,
+  };
+};
+
+const dict = [
+  "below",
+  "down",
+  "go",
+  "going",
+  "horn",
+  "how",
+  "howdy",
+  "it",
+  "i",
+  "low",
+  "own",
+  "part",
+  "partner",
+  "sit",
+];
+
+subString("below", dict);
